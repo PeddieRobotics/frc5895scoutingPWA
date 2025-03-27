89785aec6f8f21114ee967b31850219b980e8d78
Jacob Kidd <jkidd-26@peddie.org>
2025-03-26 21:22:10 -0700

    offline fixes maybe

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const withPWA = (require('next-pwa'))({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  });
+  
+  const nextConfig = withPWA({
+    // Your existing Next.js config
+    reactStrictMode: true,
+  });
+  
+  export default nextConfig;
