387f216af434215b614561bc151ba53d295fab51
zachary <one@zacharyphelan.com>
2025-04-05 11:36:04 -0400

    real thing time trust

--- src/app/form-components/TextInput.js
+++ src/app/form-components/TextInput.js
@@ -1,30 +1,16 @@
 import styles from './TextInput.module.css'
-import { useRef, useState } from 'react'
-
 export default function TextInput ({ visibleName, internalName, defaultValue, type="text", pattern, className }) {
-    // Add state to properly handle input value changes
-    const [inputValue, setInputValue] = useState(defaultValue || "");
-    const inputRef = useRef(null);
-    
-    // Handle input changes in a controlled way
-    const handleChange = (e) => {
-        setInputValue(e.target.value);
-    };
-    
     return (
         <div className={styles.TextInput}>
             <label htmlFor={internalName}>{visibleName}</label>
             <br></br>
             <input 
-                ref={inputRef}
                 className={className || ""} 
                 type={type} 
                 id={internalName} 
                 name={internalName} 
-                value={inputValue}
-                onChange={handleChange}
+                defaultValue={defaultValue}
                 pattern={pattern}
-                data-noshift="true" // Add data attribute for iOS fixes
             ></input>
             <br></br>
         </div>
