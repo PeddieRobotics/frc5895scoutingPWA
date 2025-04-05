82d02c5f622b33aa2fea4023860103b065000310
zachary <one@zacharyphelan.com>
2025-04-05 13:23:54 -0400

    favicon add

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,6 +40,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Teko&amp;display=swap" rel="stylesheet"></link>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png"></link>
         <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192.png"></link>
         <link rel="apple-touch-icon" sizes="512x512" href="/icons/icon-512.png"></link>
